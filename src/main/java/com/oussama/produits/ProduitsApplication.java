5a80917fe8987ce8f7cd51c84fc8ed4c8206ccca
unknown <oussamaferjani.dev@gmail.com>
2022-11-03 21:01:01 +0100

    commit at5

---
+++ src/main/java/com/oussama/produits/ProduitsApplication.java
@@ -0,0 +1,31 @@
+package com.oussama.produits;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import com.oussama.produits.entities.Produit;
+
+@SpringBootApplication
+public class ProduitsApplication  implements CommandLineRunner{
+
+	
+	@Autowired
+	private RepositoryRestConfiguration repositoryRestConfiguration;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		repositoryRestConfiguration.exposeIdsFor(Produit.class);
+		
+		
+	}
+
+}
---
+++ src/main/java/com/oussama/produits/entities/ProduitProjection.java
@@ -0,0 +1,9 @@
+package com.oussama.produits.entities;
+
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "nomProd", types = { Produit.class })
+public interface ProduitProjection {
+	public String getNomProduit();
+
+}
